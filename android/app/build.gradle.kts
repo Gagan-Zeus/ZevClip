33aba39a39f2fa174374e75a78e0d3fb76905176
Gagan Deep S <gaganch17210@gmail.com>
2026-06-05 19:56:14 +0530

    Bump ZevClip to 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.zevclip.sender"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1.0"
     }
 
     buildTypes {
